d4cbba5dc71ffadcd000f4a8415fc07283a3b18d
Orban Botond <orbanbotond@gmail.com>
2018-06-30 19:29:31 +0300

    The update user is done.

---
+++ app/concepts/crud/user/contracts/update.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "reform/form/dry"
+
+module Crud
+  module User
+    class Contracts::Update < Reform::Form
+      feature Reform::Form::Dry
+
+      property :admin
+
+      validation :default do
+        required( :admin).filled(:bool?)
+      end
+
+      class << self
+        def documentation
+          {
+            admin: { required: true, type: "String", desc: "Specifiying that the user is admin or not." }
+          }
+        end
+      end
+    end
+  end
+end

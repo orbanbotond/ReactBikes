daf4d088b9605cd6aeeca75a091dc22daeb938ca
Orban Botond <orbanbotond@gmail.com>
2018-07-01 01:05:07 +0300

    The availablebikes query contract is ready

--- spec/concepts/queries/contract/available_bikes_spec.rb
+++ spec/concepts/queries/contract/available_bikes_spec.rb
@@ -2,7 +2,7 @@
 
 require "rails_helper"
 
-describe AvailableBikes::Contracts::Query do
+describe Queries::Contracts::AvailableBikes do
   let(:contract) { described_class.new({}) }
   let(:params) { attributes_for(:reservation).slice(:start_date, :end_date) }
 
---
+++ spec/concepts/queries/operations/available_bikes_spec.rb
@@ -0,0 +1,7 @@
+# # frozen_string_literal: true
+
+# require "rails_helper"
+
+# describe AvailableBikes::Contracts::Query do
+#   let(:contract) { described_class.new({}) }
+#   let(:params) { attributes_for(:reservation).slice(:start_date, :end_date) }

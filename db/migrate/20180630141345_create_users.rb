d0d857357e2acc910ae6983788327c17c0929e35
Orban Botond <orbanbotond@gmail.com>
2018-06-30 17:21:37 +0300

    Kickoff Done

---
+++ db/migrate/20180630141345_create_users.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.boolean :admin
+
+      t.timestamps
+    end
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require File.expand_path('../config/environment', __dir__)
-require 'rails/test_help'
+require File.expand_path("../config/environment", __dir__)
+require "rails/test_help"
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.

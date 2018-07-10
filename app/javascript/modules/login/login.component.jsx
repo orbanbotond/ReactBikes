dd3397ab4599fd3e91c3db8f689ef304ade15691
Orban Botond <orbanbotond@gmail.com>
2018-07-11 01:22:01 +0300

    The login form validation is fine.

--- app/javascript/modules/login/login.component.jsx
+++ app/javascript/modules/login/login.component.jsx
@@ -16,7 +16,7 @@ const LoginForm = ({
   const inputSetup = {
     labelSize: 4,
     inputSize: 8,
-    // validate: [validators.required],
+    validate: [validators.required],
     disabledForm: disableForm,
   };
 
@@ -27,15 +27,17 @@ const LoginForm = ({
         autoComplete="off"
         className="col-lg-6 offset-lg-3">
         <Input
+          {...inputSetup}
           name="email"
           label="Email"
-          {...inputSetup} />
+          validate={[validators.required, validators.email]}
+        />
         <Input
           {...inputSetup}
           name="password"
           type="password"
           label="Password" />
-         <Button className="col-lg-12" outline size="sm" color="success" inactive={(dirty && invalid) || (pristine && invalid) || submitting}>Submit</Button>
+         <Button className="col-lg-12" outline size="sm" color="success" disabled={(dirty && invalid) || (pristine && invalid) || submitting}>Submit</Button>
       </Form>
     </div>
   );

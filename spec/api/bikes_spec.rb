690a09df796dc7ebb026dfdc7016b627ca408a1c
Orban Botond <orbanbotond@gmail.com>
2019-04-05 16:47:32 +0300

    Added: foreign key check for the contract. And all the negative specs for the bikes API

--- spec/api/bikes_spec.rb
+++ spec/api/bikes_spec.rb
@@ -2,33 +2,78 @@
 
 require "rails_helper"
 
-describe "GET /api/bike_models" do
-  let(:path) { "/api/bike_models" }
-  subject { get path, params: params, headers: developer_header }
+describe "bikes API endpoints" do
   let(:session_token) { 'token-xxx123123' }
   let!(:user) { create :user, hashed_session_token: User.pwd_hash(session_token) }
-  let(:params) { {} }
   let(:developer_header) { {"X-Auth-Token" => session_token} }
 
   context 'creates a bike' do
+    let(:bike_model) { create :bike_model }
+    let(:params) { { color: ::Bike::COLORS.first, weight: 12, latitude: 12.5, longitude: 13.6, bike_model_id: bike_model.id } }
+    let(:path) { "/api/bikes" }
+    subject { post path, params: params, headers: developer_header }
+
     context 'negative cases' do
       it_behaves_like "unauthenticated"
+
+      context 'invalid params' do
+        context 'color' do
+          let(:params) { super().merge color: 'Invalid Color' }
+
+          specify "Returns unauthorized" do
+            expect_bad_request
+            expect_json
+          end
+        end
+
+        context 'bike_model_id' do
+          let(:params) { super().merge bike_model_id: -1 }
+
+          specify "Returns unauthorized" do
+            expect_bad_request
+            expect_json
+          end
+        end
+      end
     end
 
     context 'positive cases' do
+      specify "Returns created" do
+        expect_created
+        expect_json
+        expect_contains_field("id")
+        expect_contains_field("bike_model_id")
+      end
     end
   end
 
   context 'returns all the bikes' do
+    let!(:bike_1) { create :bike }
+    let!(:bike_2) { create :bike }
+    let(:params) { { } }
+    let(:path) { "/api/bikes" }
+    subject { get path, params: params, headers: developer_header }
+
     context 'negative cases' do
       it_behaves_like "unauthenticated"
     end
 
     context 'positive cases' do
+      specify "Returns success" do
+        expect_success
+        expect_json
+        expect_first_in_the_array_includes("id")
+        expect_first_in_the_array_includes("bike_model_id")
+      end
     end
   end
 
-  context 'reservation of a bike' do
+  context 'reservations of a bike' do
+    let(:bike) { create :bike }
+    let(:params) { { } }
+    let(:path) { "/api/bikes/#{bike.id}/reservations" }
+    subject { get path, params: params, headers: developer_header }
+
     context 'negative cases' do
       it_behaves_like "unauthenticated"
     end
@@ -38,6 +83,11 @@ describe "GET /api/bike_models" do
   end
 
   context 'returns a bike' do
+    let(:bike) { create :bike }
+    let(:params) { { } }
+    let(:path) { "/api/bikes/#{bike.id}" }
+    subject { get path, params: params, headers: developer_header }
+
     context 'negative cases' do
       it_behaves_like "unauthenticated"
     end
@@ -47,6 +97,11 @@ describe "GET /api/bike_models" do
   end
 
   context 'deletes a bike' do
+    let(:bike) { create :bike }
+    let(:params) { { } }
+    let(:path) { "/api/bikes/#{bike.id}" }
+    subject { delete path, params: params, headers: developer_header }
+
     context 'negative cases' do
       it_behaves_like "unauthenticated"
     end
@@ -56,6 +111,11 @@ describe "GET /api/bike_models" do
   end
 
   context 'updates a bike' do
+    let(:bike) { create :bike }
+    let(:params) { { } }
+    let(:path) { "/api/bikes/#{bike.id}" }
+    subject { put path, params: params, headers: developer_header }
+
     context 'negative cases' do
       it_behaves_like "unauthenticated"
     end
--- spec/concepts/crud/bike/contracts/create_spec.rb
+++ spec/concepts/crud/bike/contracts/create_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 
 describe Crud::Bike::Contracts::Create do
   let(:contract) { described_class.new Bike.new }
-  let!(:bike_model) { build_stubbed :bike_model }
+  let!(:bike_model) { create :bike_model }
   let(:params) { { weight: 12, latitude: 54, longitude: 30, color: Bike::COLORS.first, bike_model_id: bike_model.id } }
 
   context "negative cases" do
@@ -23,6 +23,15 @@ describe Crud::Bike::Contracts::Create do
         it "fails validation" do
           expect(contract.validate params).to be_falsey
         end
+      end
+    end
+
+    context "bike_model_id is invalid" do
+      let(:params) { super().merge(bike_model_id: -1) }
+
+      it "fails validation" do
+        expect(contract.validate params).to be_falsey
+        expect(contract.errors[:bike_model_id]).to be_present
       end
     end
   end

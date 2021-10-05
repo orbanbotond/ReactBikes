eae3f565940aa9600c7af9d2851437905e6bfd2d
Orban Botond <orbanbotond@gmail.com>
2021-10-05 21:50:11 +0300

    Add N+1 query for the users/reservations

--- spec/graphql/n_plus_1_spec.rb
+++ spec/graphql/n_plus_1_spec.rb
@@ -3,30 +3,27 @@
 require "rails_helper"
 
 RSpec.describe "N+1 query test", :n_plus_one do
-  let!(:model) { BikeModel.create text: "Mountain" }
-  let!(:bike) { Bike.create weight: 1.2, color: Bike::COLORS.first, bike_model: model, latitude: 48.210033, longitude: 16.363449, average_rating: 1 }
   let(:current_user) { create :user, admin: true }
 
-  populate do |n|
-    puts "Population: #{n}"
-    n.times do
-      user = User.create email: "boti#{n}@gmail.com"
-      Reservation.create user: user, bike: bike
+  describe 'for bikes/reservations' do
+    populate do |n|
+      n.times do
+        user = create :user, email: "boti#{n}@gmail.com"
+        create :reservation, user: user
+      end
     end
-  end
 
-  it "loads the reservations" do
-    query_string = <<~GQL
-        {
-          bikes{
-            edges{
-              node {
+    it "loads the reservations using one sql" do
+      query_string = <<~GQL
+          {
+            bikes{
+              nodes {
                 id,
                 model{
                   text
                 },
                 reservations {
-                  cancelled,
+                  id,
                   user {
                     email
                   }
@@ -34,12 +31,45 @@ RSpec.describe "N+1 query test", :n_plus_one do
               }
             }
           }
+        GQL
+
+      expect do
+        context = { current_user: current_user }
+        result = ToptalReactBikesSchema.execute(query_string, context: context, variables: {})
+      end.to perform_constant_number_of_queries
+    end    
+  end
+
+  describe 'for users/reservations' do
+    populate do |n|
+      user = create :user, email: "boti#{n}@gmail.com"
+      n.times do
+        create :reservation, user: user
+      end
+    end
+
+    it "loads the reservations using one sql" do
+      query_string = <<~GQL
+        {
+          users{
+            nodes{
+              id,
+              reservations{
+                id,
+                user {
+                  email
+                }
+              }
+            }
+          }
         }
       GQL
 
-    expect do
-      context = { current_user: current_user }
-      result = ToptalReactBikesSchema.execute(query_string, context: context, variables: {})
-    end.to perform_constant_number_of_queries
+      expect do
+        context = { current_user: current_user }
+        result = ToptalReactBikesSchema.execute(query_string, context: context, variables: {})
+      end.to perform_constant_number_of_queries
+    end    
   end
+
 end

b1861055ebaddf6150ad2c350580c2b375ea0897
Orban Botond <orbanbotond@gmail.com>
2021-10-10 19:20:06 +0300

    Add require admin for the field UserType

--- app/graphql/types/base/field.rb
+++ app/graphql/types/base/field.rb
@@ -2,6 +2,16 @@ module Types
   module Base
     class Field < GraphQL::Schema::Field
       argument_class Types::Base::Argument
+
+      def initialize(*args, require_admin: false, **kwargs, &block)
+        @require_admin = require_admin
+        super(*args, **kwargs, &block)
+      end
+
+      def visible?(context)
+        # if `require_admin:` was given, then require the current user to be an admin
+        super && (@require_admin ? context[:current_user].try(:'admin?') : true)
+      end
     end
   end
 end

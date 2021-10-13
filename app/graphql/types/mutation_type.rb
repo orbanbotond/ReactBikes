4f45b00a7d12637c129c48863266e4688cdceaa3
Orban Botond <orbanbotond@gmail.com>
2021-10-13 14:13:39 +0300

    Add the delete bike mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,5 +4,6 @@ module Types
   class MutationType < Types::Base::Object
     field :create_bike, mutation: ::Mutations::CreateBike
     field :update_bike, mutation: ::Mutations::UpdateBike
+    field :delete_bike, mutation: ::Mutations::DeleteBike
   end
 end
---
+++ spec/graphql/mutations/delete_bike_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "DeleteBike" do
+  let(:bike) { create :bike }
+  let(:bike_id) { ToptalReactBikesSchema.id_from_object(bike, bike.class, {}) }
+
+  subject(:scoped_reservations) do
+    context = { current_user: current_user }
+    result = ToptalReactBikesSchema.execute(query_string, context: context, variables: variables)
+    result.to_h["data"]["deleteBike"]
+  end
+
+  let(:variables) do
+    {
+      "bikeId": bike_id
+    }
+  end
+
+  let(:query_string) do
+    <<~GQL
+      mutation DeleteBike($bikeId: ID!){
+        deleteBike(input: {bikeId: $bikeId}){
+          errors,
+        }
+      }
+    GQL
+  end
+
+  context "negative cases" do
+    context "when not authenticated" do
+      let(:current_user) { create :user }
+
+      it "returns an error" do
+        expect(subject["errors"]).to be_present
+      end
+    end
+  end
+
+  context "positive cases" do
+    context "when authenticated" do
+      let(:current_user) { create :user, :admin }
+
+      it "returns bike" do
+        expect(subject["errors"]).not_to be_present
+      end
+    end
+  end
+end

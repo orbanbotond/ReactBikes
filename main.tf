e5d556c2d71fa35bbd22d0519f694d9edbcc275b
Orban Botond <orbanbotond@gmail.com>
2022-08-11 18:25:50 +0300

    Terraform the Heroku Host

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    heroku = {
+      source = "heroku/heroku"
+      version = "~> 4.6"
+    }
+  }
+}
+
+provider "heroku" {}
+
+module "heroku" {
+  source = "/Users/orbanbotond/profession/devops/terraform-pack"
+
+  app_name  = "bike-rental-rails-flux-react"
+}

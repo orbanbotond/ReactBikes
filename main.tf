8d72306369f54da205a2c357dc4e7dde410519ad
Orban Botond <orbanbotond@gmail.com>
2022-08-11 18:49:54 +0300

    Get rid of the package lock

--- main.tf
+++ main.tf
@@ -13,4 +13,5 @@ module "heroku" {
   source = "/Users/orbanbotond/profession/devops/terraform-pack"
 
   app_name  = "bike-rental-rails-flux-react"
+  source_code_github_url = "https://github.com/orbanbotond/ReactBikes"
 }

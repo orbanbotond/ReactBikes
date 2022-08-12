4d7b8cc5a88383b027d3336805549612fef8850d
Orban Botond <orbanbotond@gmail.com>
2022-08-12 19:18:23 +0300

    Require the batchloader

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+require 'batch_loaders/association_loader'
 
 module ToptalReactBikes
   class Application < Rails::Application

545b00163ef5c4caec92942be13bc1fe4aa4cda4
Orban Botond <orbanbotond@gmail.com>
2022-08-16 19:01:02 +0300

    Remove webpacker

--- config/webpack/environment.js
+++
@@ -1,7 +0,0 @@
-const { environment } = require('@rails/webpacker')
-const sharedConfig = require('./shared');
-
-environment.config.merge(sharedConfig);
-environment.config.resolve.extensions = ['.js', '.jsx', '.json'];
-
-module.exports = environment;

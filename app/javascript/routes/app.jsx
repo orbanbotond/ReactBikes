fe030e6113715d9a7eef84411a1b68525c1393f0
Orban Botond <orbanbotond@gmail.com>
2022-08-17 11:20:29 +0300

    Add switch back

--- app/javascript/routes/app.jsx
+++ app/javascript/routes/app.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
+  Switch,
 } from 'react-router-dom';
 
 import { StandardUserGuard, AdminGuard, LoginGuard } from '../modules/route-guards'
@@ -27,30 +28,34 @@ const App = (_props) => {
                component={LoginGuard(Login)} />
         <Route path={Routes.Browser.signup()}
                component={LoginGuard(SignUp)} />
-        <Route path={Routes.Browser.Restfull.collection_route("bike")}
-               component={AdminGuard(BikeList)}
-               exact />
-        <Route path={`${Routes.Browser.Restfull.member_subroute('bike', ':id', 'reservations')}`}
-               component={AdminGuard(BikeReservations)}
-               exact />
-        <Route path={`${Routes.Browser.Restfull.member_route('bike', ':id')}`}
-               component={AdminGuard(BikeEdit)}
-               exact />
-        <Route path={Routes.Browser.Restfull.new_route('bike')}
-               component={AdminGuard(BikeNew)}
-               exact />
-        <Route path={Routes.Browser.Restfull.collection_route("user")}
-               component={AdminGuard(UserList)}
-               exact />
-        <Route path={`${Routes.Browser.Restfull.member_subroute('user', ':id', 'reservations')}`}
-               component={AdminGuard(UserReservations)}
-               exact />
-        <Route path={`${Routes.Browser.Restfull.member_route('user', ':id')}`}
-               component={AdminGuard(UserEdit)}
-               exact />
-        <Route path={Routes.Browser.Restfull.new_route('user')}
-               component={AdminGuard(UserNew)}
-               exact />
+        <Switch>
+          <Route path={Routes.Browser.Restfull.collection_route("bike")}
+                 component={AdminGuard(BikeList)}
+                 exact />
+          <Route path={`${Routes.Browser.Restfull.member_subroute('bike', ':id', 'reservations')}`}
+                 component={AdminGuard(BikeReservations)}
+                 exact />
+          <Route path={`${Routes.Browser.Restfull.member_route('bike', ':id')}`}
+                 component={AdminGuard(BikeEdit)}
+                 exact />
+          <Route path={Routes.Browser.Restfull.new_route('bike')}
+                 component={AdminGuard(BikeNew)}
+                 exact />
+        </Switch>
+        <Switch>
+          <Route path={Routes.Browser.Restfull.collection_route("user")}
+                 component={AdminGuard(UserList)}
+                 exact />
+          <Route path={`${Routes.Browser.Restfull.member_subroute('user', ':id', 'reservations')}`}
+                 component={AdminGuard(UserReservations)}
+                 exact />
+          <Route path={`${Routes.Browser.Restfull.member_route('user', ':id')}`}
+                 component={AdminGuard(UserEdit)}
+                 exact />
+          <Route path={Routes.Browser.Restfull.new_route('user')}
+                 component={AdminGuard(UserNew)}
+                 exact />
+        </Switch>
         <Route path={Routes.Browser.search()}
                component={StandardUserGuard(Search)}
                exact />

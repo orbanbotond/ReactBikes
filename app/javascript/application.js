66ea9baca192083f6bb0dba1ff75a47d0f66865f
Orban Botond <orbanbotond@gmail.com>
2022-08-17 13:06:26 +0300

    Add all dependencies

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,7 +4,6 @@
 console.info("loading the app/javascripts/application.js")
 
 import './routes/main';
-// import Rails from "@rails/ujs"
 // Rails.starts()
 
 console.info("loaded the app/javascripts/application.js")
--- app/javascript/modules/bike_rental/future-reservation.component.jsx
+++ app/javascript/modules/bike_rental/future-reservation.component.jsx
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Toggle from 'react-toggle';
 import Rater from 'react-rater'
+
 // import 'react-rater/lib/react-rater.css';
-// import "react-toggle/style.css" 
+// import 'react-toggle/style.css' 
 
 export default class Reservation extends Component {
   constructor(props) {
